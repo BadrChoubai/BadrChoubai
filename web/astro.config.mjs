9e6f2d9c3c0397d3fca0574941ac2822ed452651
Badr <choubai.badr@protonmail.com>
2023-06-20 09:53:30 -0600

    remove astro

--- web/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
-
-export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-});
