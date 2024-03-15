64c27910b2209356ddc0bb210cf43d3efccae685
Badr <choubai.badr@protonmail.com>
2024-03-15 00:13:11 -0600

    modify style

--- www/eleventy.config.js
+++ www/eleventy.config.js
@@ -1,9 +1,7 @@
 module.exports = function (eleventyConfig) {
 	// Universal Shortcodes (Adds to Liquid, Nunjucks, JavaScript, Handlebars)
 	eleventyConfig.addShortcode("post", function (postData) {
-		return `<li class="post">
-		<a href="${postData.url}">${postData.title}</a>
-	</li>`;
+		return `<li class="post"><a href="${postData.url}">${postData.title}</a></li>`;
 	});
 
 	eleventyConfig.addPassthroughCopy("src/robots.txt", "/robots.txt");
