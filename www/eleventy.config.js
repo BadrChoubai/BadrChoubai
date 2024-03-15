922ca594700f63f66bc77291bec35cb363d63632
Badr <choubai.badr@protonmail.com>
2024-03-14 22:52:57 -0600

    Improve content

--- www/eleventy.config.js
+++ www/eleventy.config.js
@@ -1,22 +1,19 @@
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("src/robots.txt", '/robots.txt')
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/robots.txt", "/robots.txt");
 
 	// Universal Shortcodes (Adds to Liquid, Nunjucks, JavaScript, Handlebars)
 	eleventyConfig.addShortcode("post", function (postData) {
 		return `<li class="post">
-		${postData.title} - ${postData.date}
+		<a href="${postData.url}">${postData.title}</a>
 	</li>`;
 	});
 
-
 	return {
-		templateFormats: [
-			"md",
-			"njk",
-		],
 		dir: {
 			input: "src",
-			output: "_site"
-		}
-	}
-}
+			output: "_site",
+		},
+		pathPrefix: "/",
+		templateFormats: ["md", "njk"],
+	};
+};
